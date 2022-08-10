4aa52c00c99ac4c5c4c83bdf0962708c83230984
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-08-10 11:28:37 +0530

    Create 108.java

---
+++ 1-500/108.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if(nums == null || nums.length == 0) return null;
+        return createTree(nums, 0, nums.length - 1);
+    }
+    private TreeNode createTree(int[] nums, int start, int end){
+        if(start > end) return null;
+        int mid = start + (end - start) / 2;
+        TreeNode node = new TreeNode(nums[mid]);
+        node.left = createTree(nums, start, mid - 1);
+        node.right = createTree(nums, mid + 1, end);
+        return node;
+    }
+}
