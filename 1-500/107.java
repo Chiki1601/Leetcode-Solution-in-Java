6e254fe3691a7b09723b4460b51512ae65a83410
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-08-24 10:51:28 +0530

    Create 107.java

---
+++ 1-500/107.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if(root == null) return res;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            List<Integer> list = new ArrayList<>();
+            int size = queue.size();
+            for(int i=0; i< size; i++){
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if(node.left != null) queue.offer(node.left);
+                if(node.right != null) queue.offer(node.right);
+            }
+            res.add(list);
+        }
+
+        Collections.reverse(res);
+
+        return res;
+    }
+}
