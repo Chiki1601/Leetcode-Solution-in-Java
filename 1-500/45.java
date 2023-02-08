d6e9a0703237712d0b0955c473665b197e7a9b4c
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2023-02-08 11:45:43 +0530

    Create 45.java

---
+++ 1-500/45.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int jump(int[] nums) {
+        if(nums.length == 1){
+            return 0;
+        }
+        int max = 0;
+        int curr = 0;
+        int count = 0;
+        for(int i = 0 ; i < nums.length - 1 ; i++){
+            max = Math.max(max , i + nums[i]);
+            if(curr == i){
+                curr = max;
+                count++;
+            }
+            if(curr>nums.length-1){
+                return count;
+            }
+        }
+        return count;
+    }
+}
