08c323e1a836b5afc8819f956eea438ad3bb5e1c
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2024-12-27 09:42:52 +0530

    Create 1014.java

---
+++ 1001-1500/1014.java
@@ -0,0 +1,11 @@
+class Solution {
+  public int maxScoreSightseeingPair(int[] values) {
+    int max = 0, score = values[0];
+    for (int i = 1; i < values.length; ++i) {
+      --score;
+      if (score + values[i] > max) max = score + values[i];
+      if (values[i] > score) score = values[i];
+    }
+    return max;
+  }
+}
