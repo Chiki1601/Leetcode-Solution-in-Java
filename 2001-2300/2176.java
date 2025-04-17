3a6dc2aa7e4fb95e08985e9a06e2387595f431d1
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2025-04-17 10:24:47 +0530

    Create 2176.java

---
+++ 2001-2300/2176.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int countPairs(int[] nums, int k) {
+        int pairs = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j] && (i * j) % k == 0) {
+                    pairs++;
+                }
+            }
+        }
+        return pairs;
+    }
+}
