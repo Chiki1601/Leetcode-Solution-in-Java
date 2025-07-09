79f44f98e6485365e75349299a2119b8b0ca6145
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2025-07-09 08:50:13 +0530

    Create 3439.java

---
+++ 3001-3500/3439.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public int maxFreeTime(int eventTime, int k, int[] startTime, int[] endTime) {
+        int count = startTime.length;
+        int max = 0;
+        int[] prefixSum = new int[count + 1];
+
+        for (int i = 0; i < count; i++) {
+            prefixSum[i + 1] = prefixSum[i] + endTime[i] - startTime[i];
+        }
+
+        for (int i = k - 1; i < count; i++) {
+            int occupied = prefixSum[i + 1] - prefixSum[i - k + 1];
+            int windowEnd = (i == count - 1) ? eventTime : startTime[i + 1];
+            int windowStart = (i == k - 1) ? 0 : endTime[i - k];
+            max = Math.max(max, windowEnd - windowStart - occupied);
+        }
+
+        return max;
+    }
+}
